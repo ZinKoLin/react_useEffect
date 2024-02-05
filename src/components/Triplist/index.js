2e077b15421f743a0d6fe8465687f3ff8c36bc7b
ZinKoLin <zinkolin113@gmail.com>
2024-02-05 19:49:06 +0630

    useEffect Dependency Array  Example

--- src/components/Triplist/index.js
+++ src/components/Triplist/index.js
@@ -4,22 +4,26 @@ function Index() {
 
     let [trips, setTrips] = useState([]);
 
+    let [url, setUrl]= useState('http://localhost:3000/trips');
+
     
 
     useEffect(()=>{
-        fetch('http://localhost:3000/trips')
+        fetch(url)
         .then(res=>res.json())
         .then(data=>{
             setTrips(data)
        
     })
-    },[]);//dependcy arrary
+    },[url]);//dependcy arrary
 
     console.log(trips)
 
   return (
     <div>
       <h1>Ready To go</h1>
+      <button onClick={()=>setUrl('http://localhost:3000/trips')}>All Trip</button>
+      <button onClick={()=>setUrl('http://localhost:3000/trips?country=Myanmar')}>Trip Myanmar</button>
 
       <ul>
         {trips.map(trip=>(
